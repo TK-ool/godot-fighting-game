6a091775cb732a1da613afcf9d5bbee62b119b27
Tom <58178247+TK-ool@users.noreply.github.com>
2026-05-05 19:27:43 +0200

    Bullet hits and health

---
+++ Ressources/HealthRessource.gd
@@ -0,0 +1,21 @@
+extends Resource
+class_name HealthResource
+
+signal died
+signal hit
+
+@export var max_health: int = 100
+@export var current_health: int = 100
+
+func _init() -> void:
+	current_health = max_health
+
+func set_health(value):
+	current_health = value
+	if current_health <= 0:
+		died.emit()
+
+func take_damage(amount: float):
+	set_health(current_health-amount)
+	print(current_health)
+	hit.emit()
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,6 +3,13 @@ extends Node2D
 
 const Speed: int = 600
 
+func _ready():
+	add_to_group("bullet")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += transform.x * Speed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	self.queue_free()
