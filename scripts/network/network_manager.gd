c75088887be4558e4a1bfbf969a85c67c40ea787
Tom <58178247+TK-ool@users.noreply.github.com>
2026-05-06 15:52:43 +0200

    init multiplayer
    - added create server
    - added create host
    - created test message using RPC
    - added server disconnect signals

---
+++ scripts/network/network_manager.gd
@@ -0,0 +1,29 @@
+extends Node
+
+const SERVER_PORT: int = 8080
+
+#creates a server for clients to connect to
+func create_server():
+	var enet_network_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	enet_network_peer.create_server(SERVER_PORT)
+	get_tree().get_multiplayer().multiplayer_peer = enet_network_peer
+	print("Server created!")
+
+#connects to the specified server
+func create_client(host_ip: String = "localhost", host_port: int = SERVER_PORT):
+	_setup_client_connection_signals()
+	
+	var enet_network_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	enet_network_peer.create_client(host_ip, host_port)
+	get_tree().get_multiplayer().multiplayer_peer = enet_network_peer
+	print("Client created!")
+
+#connects disconnection signal if not already connected
+func _setup_client_connection_signals():
+	if not get_tree().get_multiplayer().server_disconnected.is_connected(_server_disconnected):
+		get_tree().get_multiplayer().server_disconnected.connect(_server_disconnected)
+
+#Fires when signal sees that host disconnects
+func _server_disconnected():
+	print("Server Disconnected")
+	get_tree().get_multiplayer().multiplayer_peer = null
