6b8b04141364541de3d18629e08e4b36731dc227
Tom <58178247+TK-ool@users.noreply.github.com>
2026-05-08 17:00:20 +0200

    Revert "se prev"
    This reverts commit 353372c1fef13ee0c4355f09d71e74cdc41506e1.

---
+++ Ressources/BulletResource.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name BulletResource 
+
+@export var speed_modifier: float = 1.0 #mulitplicative speed modifier of the projectile
+@export var size_modifier: float = 1.0 #multplicative size modifier of the projectile
+@export var damage_modifier: int = 1  #multplicative damage modifier of the projectile
+@export var amount_modifier: int = 1 #additive bullet amount modifier
+@export var spread_modifier: float = 1.0 #additive spread modifier if multiple bullets
+@export var duration_modifier: float = 1.0 #multiplicative time till respawn modifier
