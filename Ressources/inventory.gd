7f2a3605082defedf8a424ce14374b8e87d568d0
Tom <58178247+TK-ool@users.noreply.github.com>
2026-05-27 22:13:29 +0200

    fixed inventory only allows 4 items

--- Ressources/inventory.gd
+++ Ressources/inventory.gd
@@ -2,9 +2,11 @@ class_name Inventory
 extends Resource
 
 @export var cards: Array[WeaponResource] = []
+const MAX_SIZE = 4
 
 func add_card(weapon: WeaponResource):
-	cards.append(weapon)
+	if cards.size() < MAX_SIZE:
+		cards.append(weapon)
 
 func remove_card(weapon: WeaponResource):
 	cards.erase(weapon)
