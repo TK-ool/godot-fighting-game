687cb9d9abd18c7f568fd835b7a5249d146ba9c3
marvinbschulz12 <73615984+marvinbschulz12@users.noreply.github.com>
2026-06-12 20:05:57 +0200

    splitgun angle spawn fix

--- Ressources/Behaviours/BounceBehaviour.gd
+++ Ressources/Behaviours/BounceBehaviour.gd
@@ -1,7 +1,8 @@
 class_name BounceBeahviour
 extends BulletBehaviour
 
-@export var bullet_bounces: int = 3
+@export var dict = {bullet_bounces = 3}
+var duplicate = dict.duplicate(true)
 
 func on_tick(bullet: Node, delta: float):
 	var collision = bullet.move_and_collide(bullet.velocity * delta)
@@ -9,10 +10,10 @@ func on_tick(bullet: Node, delta: float):
 		var reflect =collision.get_remainder().bounce(collision.get_normal())
 		bullet.velocity = bullet.velocity.bounce(collision.get_normal())
 		bullet.move_and_collide(reflect)
+		duplicate["bullet_bounces"] -= 1
+	if duplicate["bullet_bounces"] == 0:
+		bullet.queue_free()
 
-
-
-
-
+	
 func on_ready(bullet: Node):
 	bullet.velocity = Vector2(bullet.speed, 0).rotated(bullet.global_rotation)
--- Ressources/Behaviours/SplitBehaviour.gd
+++ Ressources/Behaviours/SplitBehaviour.gd
@@ -1,21 +1,23 @@
 class_name SplitBehaviour
 extends BulletBehaviour
 
-@export var sub_bullet_count: int = 15
+@export var sub_bullet_count: int = 10
 @export var sub_bullet_speed: float = 200.0
-
+@export_range(0, 360, 0.1, "radians_as_degrees") var arc : float = 170
 
 func on_wall_hit(bullet: Node2D):
-	for i in sub_bullet_count:
-		var angle = (TAU / sub_bullet_count) * i
-		var direction = Vector2.from_angle(angle)
-		var b = bullet.duplicate()
-		b.position = bullet.position + direction * 20.0
-		b.rotation = angle
-		b.device = bullet.device # sonst defaultet er zu spieler 0
-		bullet.get_parent().add_child.call_deferred(b)
-	bullet.queue_free()
-	return true
+	if bullet.collision_result != null:
+		var collision_normal = bullet.collision_result.get_normal() # normal zeigt in die gegenüberliegende richtung der wand collision
+		for i in sub_bullet_count:
+			var direction = collision_normal.angle() # convertiert die normal zu einem winkel
+			var b = bullet.duplicate()
+			var angle_increment = arc / (sub_bullet_count - 1) #  abstand zwischen den kugeln
+			b.position = bullet.position + collision_normal * 3 # collision_normal * 3 damit abstand zur wand ist sonst spawnen die kugeln nicht korrekt bei starkem winkel zur wand
+			b.global_rotation = direction + angle_increment * i - arc / 2 #spreaded die kugeln gleichmäßig über den angle
+			b.device = bullet.device # sonst defaultet er zu spieler 0
+			bullet.get_parent().add_child.call_deferred(b)
+		bullet.queue_free()
+		return true
 
 		
 func on_tick(bullet: Node, delta: float):
